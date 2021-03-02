5a339a79586d77269a1686578ec97933ffda6afb
DiegoMutre <tysonmutre10@gmail.com>
2021-03-02 07:18:56 -0500

    feat: Finish SearchForm.js structure

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,6 +6,20 @@ const SearchForm = () => {
             <Form.Row className="align-items-end">
                 <Form.Group as={Col}>
                     <Form.Label>Description</Form.Label>
+                    <Form.Control name="description" type="text" />
+                </Form.Group>
+                <Form.Group as={Col}>
+                    <Form.Label>Location</Form.Label>
+                    <Form.Control name="location" type="text" />
+                </Form.Group>
+                <Form.Group as={Col} xs="auto" className="ml-2">
+                    <Form.Check
+                        name="full-time"
+                        id="full-time"
+                        label="Only Full Time"
+                        type="checkbox"
+                        className="mb-2"
+                    />
                 </Form.Group>
             </Form.Row>
         </Form>
