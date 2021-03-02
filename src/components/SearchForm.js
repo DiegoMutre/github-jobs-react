6df8667661d76b993ea5ed5fc6c72517254f5aa0
DiegoMutre <tysonmutre10@gmail.com>
2021-03-02 08:01:07 -0500

    feat: Use handleParamChanges and params props in SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,15 +7,26 @@ const SearchForm = ({ params, handleParamChanges }) => {
             <Form.Row className="align-items-end">
                 <Form.Group as={Col}>
                     <Form.Label>Description</Form.Label>
-                    <Form.Control name="description" type="text" />
+                    <Form.Control
+                        value={params.description}
+                        onChange={handleParamChanges}
+                        name="description"
+                        type="text"
+                    />
                 </Form.Group>
                 <Form.Group as={Col}>
                     <Form.Label>Location</Form.Label>
-                    <Form.Control name="location" type="text" />
+                    <Form.Control
+                        value={params.location}
+                        onChange={handleParamChanges}
+                        name="location"
+                        type="text"
+                    />
                 </Form.Group>
                 <Form.Group as={Col} xs="auto" className="ml-2">
                     <Form.Check
-                        name="full-time"
+                        onChange={handleParamChanges}
+                        name="full_time"
                         id="full-time"
                         label="Only Full Time"
                         type="checkbox"
