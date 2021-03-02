c254ba211701cd88acedbdba89060e4faad785ad
DiegoMutre <tysonmutre10@gmail.com>
2021-03-02 08:18:07 -0500

    feat: Create useFetchJobs.js

---
+++ src/hooks/useFetchJobs.js
@@ -0,0 +1,11 @@
+const ACTIONS = {
+    MAKE_REQUEST: "make-request",
+    GET_DATA: "get-data",
+    ERROR: "error",
+    UPDATE_HAS_NEXT_PAGE: "update-has-next-page",
+};
+
+const BASE_URL =
+    "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+
+export default function useFetchJobs(params, page) {}
