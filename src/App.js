f332c611dfbbf0cb0897e5f5ca41952f9243b85a
DiegoMutre <tysonmutre10@gmail.com>
2021-03-02 07:22:13 -0500

    feat: Create params state in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import SearchForm from "./components/SearchForm";
 
 function App() {
+    const [params, setParams] = useState({});
+
     return (
         <Container className="my-4">
             <h1 className="mb-4">Github Jobs</h1>
